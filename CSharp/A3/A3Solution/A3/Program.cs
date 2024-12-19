ab1e1f0f21fe4d29c857ec7ade01f55176aee91d
Soumaya Ghaith <somayaghaith18@gmail.com>
2024-12-19 02:43:32 +0200

    Q1 C# Assignment3

---
+++ CSharp/A3/A3Solution/A3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 - Write a program that allows the user to enter a number then print it
+            //Console.WriteLine("Please enter a number to print it");
+            //int num;
+            //num =Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num);
+            #endregion
+        }
+    }
+}
