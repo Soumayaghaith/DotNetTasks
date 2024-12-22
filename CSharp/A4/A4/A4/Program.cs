01ce4b97c6cd10e6b4f9be7c2309371835eb6562
Soumaya Ghaith <somayaghaith18@gmail.com>
2024-12-23 01:56:39 +0200

    Q1 C# A4

---
+++ CSharp/A4/A4/A4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace A4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //- Write a program that takes a number from the user then print yes if
+            //that number can be divided by 3 and 4 otherwise print no.
+            //Console.Write("Please enter a number: ");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //if ((num % 3 == 0) && (num % 4 == 0))
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            #endregion
+        }
+    }
+}
