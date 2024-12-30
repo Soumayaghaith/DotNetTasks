7d4451953d485b6fc847ae02e93edbbfcdb4b1d8
Soumaya Ghaith <somayaghaith18@gmail.com>
2024-12-30 23:01:13 +0200

    Q1 C# OOPA1

---
+++ OOP/OOPA1/OOPA1/OOPA1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace OOPA1
+{
+    enum WeekDays
+    { 
+        Sat,Sun,Mon,Tue,Wed,Thurs,Fri
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //1. Create an enum called "WeekDays" with the days of the week (Monday to Sunday)
+            //as its members.Then, write a C# program that prints out all the days of the weekusing this enum.
+            //Console.WriteLine("The days of the week are:");
+            //for (int i=0; i<=6;i++)
+            //{
+            //    WeekDays day = (WeekDays)i;
+            //    Console.WriteLine(day);
+            //}
+            #endregion
+        }
+    }
+}
