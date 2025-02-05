b4115974e722c0265e48ecf79c7f0f8e52e4e5a6
Soumaya Ghaith <somayaghaith18@gmail.com>
2025-02-05 16:36:06 +0200

    ADVC# A3

---
+++ AdvancedC#/AdvancedC#A3/A3/A3/LibraryEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3
+{
+    delegate string MyDelegate(Book B);
+    internal class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList, MyDelegate func)
+        {
+            foreach (Book B in bList)
+                Console.WriteLine(func(B));
+
+        }
+        
+       
+    }
+}
---
+++ AdvancedC#/AdvancedC#A3/A3/A3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>() 
+            {
+                new Book ("1", "Book1", new string[] { "Ahmed", "Mohamed"}, new DateTime(2025,5,11), 250),
+                new Book ("2", "Book2", new string[] { "Sarah", "Moataz"}, new DateTime(2022,1,11), 150),
+                new Book ("3", "Book3", new string[] { "Ayman", "Magdy"}, new DateTime(2021,3,11), 50)
+
+            };
+            MyDelegate getTitle = BookFunctions.GetTitle;
+            Func<Book,String> func01 = BookFunctions.GetTitle;
+            LibraryEngine.ProcessBooks(books, getTitle);
+            //using anonymous methods
+            LibraryEngine.ProcessBooks(books, delegate(Book b) { return b.Title; });
+            //using lambda expression
+            LibraryEngine.ProcessBooks(books, b =>  b.Title);
+
+            MyDelegate getAuthors = BookFunctions.GetAuthors;
+            Func<Book, String> func02 = BookFunctions.GetAuthors;
+            LibraryEngine.ProcessBooks(books, getAuthors);
+            
+            //using anonymous methods
+            LibraryEngine.ProcessBooks(books, delegate (Book b) { return string.Join(",", b.Authors); });
+            //using lambda expression
+            LibraryEngine.ProcessBooks(books, b => string.Join(",", b.Authors));
+
+
+            MyDelegate getPrice = BookFunctions.GetPrice;
+            Func<Book, String> func03 = BookFunctions.GetPrice;
+            LibraryEngine.ProcessBooks(books, getPrice);
+            //using anonymous methods
+            LibraryEngine.ProcessBooks(books, delegate (Book b) { return b.Price.ToString(); });
+            //using lambda expression
+            LibraryEngine.ProcessBooks(books, b => b.Price.ToString());
+
+
+        }
+    }
+}
